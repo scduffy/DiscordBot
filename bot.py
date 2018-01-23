762f7891fe5c3737b2cda90d870d648a60d3a9a9
scduffy <scduffy@loyola.edu>
2018-01-23 13:09:09 -0500

    about finished

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import time
 
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = ".")
+client = commands.Bot(command_prefix=".")
 
 @client.event
 async def on_ready():
@@ -16,6 +16,9 @@ async def on_ready():
 async def on_message(message):
     if message.content == "nick":
         await client.send_message(message.channel, "posts cancer")
-
+    elif message.author.id == "344194195344588810":
+        if len(message.content) >= 200:
+            await client.delete_message(message)
+            await client.send_message(message.channel, "Message has been auto-deleted due to the detection of excess autism.")
 
 client.run("NDA1NDE3MDI2ODk5ODA0MTY2.DUkGkA.mFjXHKhV21rwx5HlIbpfw07eub8")
