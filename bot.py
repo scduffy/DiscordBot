c5c8c1b6ceb7529802d42d0b36bc72b2a741f737
scduffy <scduffy@loyola.edu>
2018-01-23 14:40:18 -0500

    !msg_len

--- bot.py
+++ bot.py
@@ -16,13 +16,25 @@ async def on_ready():
 # Scans all messages posted on server.
 @client.event
 async def on_message(message):
+    message_length = 200
     # If someone posts just nick in lowercase the bot will respond with "posts cancer".
     if message.content == "nick":
         await client.send_message(message.channel, "posts cancer")
+
+    elif message.content.startswith('!msg_len'):
+        args = message.content.split(" ")
+
+        if not args[1].isdigit():
+            await client.sent_message(message.channel, "Argument not valid, not a digit.")
+        else:
+            args = int(args[1])
+            await client.send_message(message.channel, "Changed Nick's max message length.")
+            message_length = args
+
     # Checks the author of the post is nick.
-    elif message.author.id == "344194195344588810":
+    elif message.author.id == "212286463642042369":
         # Checks if the length of the message exceeds 200 characters (can be changed later to less).
-        if len(message.content) >= 200:
+        if len(message.content) >= message_length:
             # Deletes message and notifies server of deletion.
             await client.delete_message(message)
             await client.send_message(message.channel, "Message has been auto-deleted due to the detection of excess autism.")
